30fa6d1bdbb46aec30fb3fc86a562af0072d429f
Nikolay Mendyaev <newbiecraft@gmail.com>
2015-02-02 02:17:36 +0300

    add test for browsers

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,12 +8,22 @@ module.exports = function(grunt) {
                     //key: 'saucelabs-key', // if not provided it'll default to ENV SAUCE_ACCESS_KEY (if applicable)
                     urls: ['somenamedevteam.github.io/JS-Logger/tests/example_cache.html'],
                     build: process.env.CI_BUILD_NUMBER,
-                    testname: 'Check Errors',
-                    browsers: [{
-                        browserName: 'firefox',
-                        version: '19',
-                        platform: 'XP'
-                    }]
+                    testname: 'Test Catch Errors',
+                    browsers: [
+                        ["Windows 8", "internet explorer", "10"],
+                        ["OS X 10.10", "safari", "8"],
+                        ["OS X 10.9", "safari", "7"],
+                        ["Windows 7", "internet explorer","8"],
+                        ["Windows 7", "internet explorer","9"],
+                        ["Windows 7", "internet explorer","10"],
+                        ["Windows 7", "firefox", "35"],
+                        ["Windows 7", "firefox", "30"],
+                        ["Windows 7", "firefox", "20"],
+                        ["Windows 7", "chrome", ""],
+                        ["Windows XP", "internet explorer", "8"],
+                        ["OS X 10.10", "iPhone", "8.0"],
+                        ["OS X 10.10", "iPhone", "8.1"]
+                    ]
                 }
             }
         }
